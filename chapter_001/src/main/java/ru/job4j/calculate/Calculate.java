c12bf90a22445aba6cebc77fc786ff1c7044c070
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-15 00:45:16 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate
+* @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru)
+*/
 public class Calculate {
+	/**
+	* Method main
+	* @param args - args
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
