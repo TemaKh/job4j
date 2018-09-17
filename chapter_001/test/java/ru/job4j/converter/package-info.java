3d224d5e410365d6c797cabdafe3d215ea6cb747
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-18 00:18:08 +0300

    3.2. Конвертер валюты.

---
+++ chapter_001/test/java/ru/job4j/converter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for converter test task.
+ *
+ * @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru).
+ * @version $Id$.
+ * @since 17.09.2018.
+ */
+package ru.job4j.converter;
