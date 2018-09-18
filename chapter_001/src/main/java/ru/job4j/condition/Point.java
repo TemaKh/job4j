5288db3cd5c69ae4fa41ba68323fa9e0333545fd
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-18 23:47:20 +0300

    3.4. Расстояние между точками в системе координат

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+/**
+ * @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for condition task.
+ *
+ * @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru)
+ * @version $Id$
+ * @since 18.09.2018
+ */
+package ru.job4j.condition;
