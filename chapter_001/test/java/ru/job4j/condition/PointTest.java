dac25a0711d296eef3207c26116b4cebed730217
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-19 21:57:02 +0300

    Test for task 3.4. Расстояние между точками в системе координат

---
+++ chapter_001/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    @Test
+    public void manWeight() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        double result = a.distanceTo(b);
+        assertThat(result, closeTo(4.47213595499958, 0.1));
+    }
+}
---
+++ chapter_001/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for point test task.
+ *
+ * @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru).
+ * @version $Id$.
+ * @since 19.09.2018.
+ */
+package ru.job4j.condition;
