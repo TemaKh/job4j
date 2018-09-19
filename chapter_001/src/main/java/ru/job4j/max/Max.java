0d07efdac36de0ffe971fc091a87d34a3de4deef
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-19 23:13:02 +0300

    4.2. Максимум из двух чисел

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+
+/**
+ * @author Artem Khizhniakov (mailto:artem_khizhniakov@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Находит максимум из двух чисел.
+     * @param first первое число.
+     * @param second второе число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
