df02c328eed71492b22d9874dcd27dbe9425005f
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-23 21:47:02 +0300

    6.5. Создать программу для сортировки массива методом перестановки.

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * Сортировка массива.
+ */
+public class BubbleSort {
+    /**
+     * Соритровка массива целых чисел, используя алгоритм сортировки пузырьком.
+     * @param array неосортированный массив.
+     * @return отсортированный массив.
+     */
+    public int[] sort(int[] array) {
+        for (int out = array.length - 1; out > 1; out--) {
+            for (int in = 0; in < out; in++) {
+                if (array[in] > array[in + 1]) {
+                    int temp = array[in];
+                    array[in] = array[in + 1];
+                    array[in + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
