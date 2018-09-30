d8a04ec5261952a612018b948e7f4a1a4825ed65
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-09-30 21:41:07 +0300

    Исправил. Слияние двух отсортированных массивов, метод определяющий отсортирован ли массив.

--- chapter_001/test/java/ru/job4j/array/MergeTest.java
+++ chapter_001/test/java/ru/job4j/array/MergeTest.java
@@ -22,4 +22,54 @@ public class MergeTest {
         int[] expect = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenOneSortArrayMoreMergeTwoSortArrayThenThreeSortArray() {
+        Merge merge = new Merge();
+        int[] oneArray = {1, 3, 5, 7, 9, 11, 12, 13, 14};
+        int[] twoArray = {2, 4, 6, 8, 10};
+        int[] result = merge.mergeArray(oneArray, twoArray);
+        int[] expect = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenOneSortArrayMergeTwoSortArrayMoreThenThreeSortArray() {
+        Merge merge = new Merge();
+        int[] oneArray = {1, 3, 5, 7, 9};
+        int[] twoArray = {2, 4, 6, 8, 10, 11, 12, 13, 14};
+        int[] result = merge.mergeArray(oneArray, twoArray);
+        int[] expect = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenOneArrayEmptyMergeTwoSortArrayThenThreeSortArray() {
+        Merge merge = new Merge();
+        int[] oneArray = new int[0];
+        int[] twoArray = {2, 4, 6, 8, 10};
+        int[] result = merge.mergeArray(oneArray, twoArray);
+        int[] expect = {2, 4, 6, 8, 10};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenOneSortArrayMergeTwoArrayEmptyThenThreeSortArray() {
+        Merge merge = new Merge();
+        int[] oneArray = {1, 3, 5, 7, 9};
+        int[] twoArray = new int[0];
+        int[] result = merge.mergeArray(oneArray, twoArray);
+        int[] expect = {1, 3, 5, 7, 9};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenOneArrayEmptyMergeTwoArrayEmptyThenThreeSortArray() {
+        Merge merge = new Merge();
+        int[] oneArray = new int[0];
+        int[] twoArray = new int[0];
+        int[] result = merge.mergeArray(oneArray, twoArray);
+        int[] expect = {};
+        assertThat(result, is(expect));
+    }
 }
