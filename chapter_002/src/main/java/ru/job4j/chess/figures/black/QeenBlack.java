597ada80e5030941ab21b8f9da6dfd5d01ab5cb5
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-10-29 22:22:56 +0300

    Каркас шахматной доски

---
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/QeenBlack.java
@@ -0,0 +1,27 @@
+package ru.job4j.chess.figures.black;
+
+import ru.job4j.chess.figures.Cell;
+import ru.job4j.chess.figures.Figure;
+
+public class QeenBlack implements Figure {
+    private final Cell position;
+
+    public QeenBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        return new Cell[] { dest };
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new QeenBlack(dest);
+    }
+}
