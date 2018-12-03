c63143880db3dc30ced04daefda873230727999b
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-12-03 22:45:58 +0300

    1. Все задачи из 3 части переделать на Stream API.

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,6 +2,7 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PhoneDictionary {
     private List<Person> persons = new ArrayList<Person>();
@@ -16,13 +17,10 @@ public class PhoneDictionary {
      * @return Список подошедших пользователей.
      */
     public List<Person> find(String key) {
-        List<Person> result = new ArrayList<>();
-        for (Person person : this.persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key) || person.getPhone().contains(key)
-                    || person.getAddress().contains(key)) {
-                result.add(person);
-            }
-        }
-        return result;
+        return persons.stream().filter(person -> person.getName().contains(key)
+                || person.getSurname().contains(key)
+                || person.getPhone().contains(key)
+                || person.getAddress().contains(key))
+                .collect(Collectors.toList());
     }
 }
--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -8,26 +8,18 @@ public class SortUser {
     }
 
     public List<User> sortNameLength(List<User> list) {
-        Collections.sort(list, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return Integer.compare(o1.getName().length(), o2.getName().length());
-            }
-        });
+        Collections.sort(list, ((o1, o2) -> (Integer.compare(o1.getName().length(), o2.getName().length()))));
         return list;
     }
 
     public List<User> sortByAllFields(List<User> list) {
-        Collections.sort(list, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
+        Collections.sort(list, (o1, o2) -> {
                 int i = o1.getName().compareTo(o2.getName());
                 if (i == 0) {
                     return o1.compareTo(o2);
                 } else {
                     return i;
                 }
-            }
         });
         return list;
     }
