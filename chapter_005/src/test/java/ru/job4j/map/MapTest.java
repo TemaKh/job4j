a288bdc61e43ea91e167ba54d23987f64bb97302
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2018-12-25 21:35:55 +0300

    4. Переопределить только equals

--- chapter_005/src/test/java/ru/job4j/map/MapTest.java
+++ chapter_005/src/test/java/ru/job4j/map/MapTest.java
@@ -12,7 +12,7 @@ public class MapTest {
         Map<User, Object> map = new HashMap<>();
         User user1 = new User("Tom", 0, new GregorianCalendar(1995, 4, 20));
         User user2 = new User("Tom", 0, new GregorianCalendar(1995, 4, 20));
-        map.put(user1 , new Object());
+        map.put(user1, new Object());
         map.put(user2, new Object());
         System.out.println(user1.equals(user2));
         System.out.println(map);
