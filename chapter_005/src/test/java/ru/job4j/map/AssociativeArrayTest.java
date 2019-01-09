e11b752b4c9449f9c6c8747469d7f2c94d26d189
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2019-01-10 00:18:49 +0300

    Правки в AssociativeArrayTest

--- chapter_005/src/test/java/ru/job4j/map/AssociativeArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/map/AssociativeArrayTest.java
@@ -22,12 +22,12 @@ public class AssociativeArrayTest {
     @Before
     public void setUp() {
         associativeArray = new AssociativeArray<>();
-        user1 = new User("Tom", 0, new GregorianCalendar(1995, 4, 20));
-        user2 = new User("Kate", 1, new GregorianCalendar(1990, 7, 23));
-        user3 = new User("Anton", 4, new GregorianCalendar(1967, 11, 13));
-        user4 = new User("Roma", 1, new GregorianCalendar(1997, 8, 6));
-        user5 = new User("Petr", 1, new GregorianCalendar(1987, 5, 17));
-        user6 = new User("Artur", 2, new GregorianCalendar(1980, 10, 27));
+        user1 = new User("Tom J", 0, new GregorianCalendar(1995, 4, 20));
+        user2 = new User("Kate H", 1, new GregorianCalendar(1990, 7, 23));
+        user3 = new User("Anton D", 4, new GregorianCalendar(1967, 11, 13));
+        user4 = new User("Roman M", 1, new GregorianCalendar(1997, 8, 6));
+        user5 = new User("Petr A", 1, new GregorianCalendar(1987, 5, 17));
+        user6 = new User("Artur N", 2, new GregorianCalendar(1980, 10, 27));
         associativeArray.insert(user1, 1);
         associativeArray.insert(user2, 2);
         iterator = associativeArray.iterator();
