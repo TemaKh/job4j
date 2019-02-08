d256a634e9b92683dc14cf82f4ed15dc3d9ef895
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2019-02-08 21:57:47 +0300

    1. Реализовать ThreadPool. Доработка теста

--- chapter_006/src/test/java/ru/job4j/pool/ThreadPoolTest.java
+++ chapter_006/src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -3,10 +3,14 @@ package ru.job4j.pool;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class ThreadPoolTest {
     private ThreadPool pool;
-
-
+    private CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>();
+    private final int value = 1;
 
     @Before
     public void setUp() {
@@ -15,11 +19,12 @@ public class ThreadPoolTest {
 
     @Test
     public void whenWork() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 4; i++) {
             pool.work(() ->
-                System.out.println(Thread.currentThread().getName())
+                list.add(value)
             );
         }
         pool.shutdown();
+        assertThat(list.size(), is(4));
     }
 }
