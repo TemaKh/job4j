2c9265facd4c927738c3e7a41e80ec9385d50cdc
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2019-02-13 20:26:53 +0300

    Корректировка ThreadPoolTest

--- chapter_006/src/test/java/ru/job4j/pool/ThreadPoolTest.java
+++ chapter_006/src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -24,6 +24,11 @@ public class ThreadPoolTest {
                 list.add(value)
             );
         }
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         pool.shutdown();
         assertThat(list.size(), is(4));
     }
