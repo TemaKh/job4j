c82e034246335a35d9b1f550be26fa2679768edd
Artem Khizhniakov <artem_khizhniakov@mail.ru>
2019-02-28 23:50:58 +0300

    Хранилище машин

---
+++ chapter_007/src/main/java/ru/job4j/car/car.sql
@@ -0,0 +1,61 @@
+﻿CREATE TABLE car_body (
+	id serial primary key,
+	body_type varchar(700)
+);
+
+CREATE TABLE engine (
+	id serial primary key,
+	engine_type varchar(700)
+);
+
+CREATE TABLE transmission (
+	id serial primary key,
+	transmission_type varchar(700)
+);
+
+CREATE TABLE cars (
+	id serial primary key,
+	model varchar(700),
+	body_id integer references car_body(id),
+	engine_id integer references engine(id),
+	transmission_id integer references transmission(id)
+);
+
+INSERT INTO car_body (body_type) VALUES
+('Sedan'),
+('Pickup'),
+('Hatchback');
+
+INSERT INTO engine (engine_type) VALUES
+('Disel'),
+('Benzine'),
+('Turbo');
+
+INSERT INTO transmission (transmission_type) VALUES
+('Automatic'),
+('Mechanics'),
+('Robotic');
+
+INSERT INTO cars (model, body_id, engine_id, transmission_id) VALUES
+('BMW', 3, 1, 1),
+('Mitsubishi', 1, 2, 2);
+
+--Вывести список всех машин и все привязанные к ним детали.
+SELECT c.model, cb.body_type, e.engine_type, t.transmission_type FROM cars AS c
+INNER JOIN car_body AS cb ON c.body_id = cb.id
+INNER JOIN engine AS e ON c.engine_id = e.id
+INNER JOIN transmission AS t ON c.transmission_id = t.id;
+
+--Вывести отдельно детали, которые не используются в машине, кузова, двигатели, коробки передач.
+
+SELECT cb.body_type FROM car_body AS cb
+LEFT JOIN cars AS c ON c.body_id = cb.id
+WHERE c.model is null;
+
+SELECT e.engine_type FROM engine AS e
+LEFT JOIN cars AS c ON c.body_id = e.id
+WHERE c.model is null;
+
+SELECT t.transmission_type FROM transmission AS t
+LEFT JOIN cars AS c ON c.body_id = t.id
+WHERE c.model is null;
--- chapter_007/src/main/java/ru/job4j/filter/filter.sql
+++ chapter_007/src/main/java/ru/job4j/filter/filter.sql
@@ -1,15 +1,23 @@
+--Написать запрос получение всех продуктов с типом "СЫР".
 SELECT * FROM product INNER JOIN type ON product.type_id = type.id WHERE type.name = 'СЫР';
 
+--Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное".
 SELECT * FROM product WHERE name LIKE '%мороженное%';
 
+--Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 SELECT * FROM product WHERE expired_date > date_trunc('month', now()) + ('1 month');
 
+--Написать запрос, который выводит самый дорогой продукт.
 SELECT * FROM product WHERE price = (SELECT max(price) FROM product);
 
+--Написать запрос, который выводит количество всех продуктов определенного типа.
 SELECT type.name, count(product.id) FROM product INNER JOIN type ON product.type_id = type.id GROUP BY type.name;
 
+--Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО".
 SELECT * FROM product INNER JOIN type ON product.type_id = type.id WHERE type.name IN ('СЫР', 'МОЛОКО');
 
+--Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
 SELECT type.name FROM product INNER JOIN type ON product.type_id = type.id GROUP BY type.name HAVING count(product.id) < 10;
 
+Вывести все продукты и их тип.
 SELECT product.name, type.name FROM product INNER JOIN type ON product.type_id = type.id;
